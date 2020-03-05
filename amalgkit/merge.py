c25f96c4722126eec4e1d9d5a45ccfc96b74ef8f
Hego_CCTB <matthias_freund@outlook.com>
2020-03-05 13:24:06 +0100

    - changed name of function in `merge` to avoid conflict with built-in funciton
    Signed-off-by: Hego_CCTB <matthias_freund@outlook.com>

--- amalgkit/merge.py
+++ amalgkit/merge.py
@@ -2,22 +2,28 @@ import pandas
 from glob import glob
 import os
 
-def read_4th(fn,column,mode):
+
+def read_4th(fn, column, mode):
+
     return pandas.read_csv(fn, delim_whitespace=1, usecols=[0,column], index_col=0, header=0,names=[mode,'length_'+fn,'eff-length_'+fn, 'est-counts_'+fn, 'tpm_'+fn])
 
-def exec(args,mode):
+
+def exec_merge(args,mode):
+
     if mode == 'est_counts':
         column = 3
     if mode == 'eff_length':
         column = 2
     files = glob('*.tsv')
 
-    sra_file = pandas.concat([read_4th(fn,column,mode) for fn in files], axis=1)
+    sra_file = pandas.concat([read_4th(fn, column, mode) for fn in files], axis=1)
     sra_file = sra_file.rename(columns={col: col.split('_')[1] for col in sra_file.columns})
     sra_file.to_csv('./merged_file/'+mode+'_'+args.out_name+'.tsv', sep='\t')
 
+
 def merge_main(args):
+
     if not os.path.exists(os.path.join(args.work_dir+'merged_file')):
         os.makedirs(os.path.join(args.work_dir+'merged_file'))
-    exec(args, "est_counts")
-    exec(args, "eff_length")
+    exec_merge(args, "est_counts")
+    exec_merge(args, "eff_length")
