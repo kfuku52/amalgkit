504dde7c5a8caed02bf4cdb831ca80d1a5189ce1
Hego_CCTB <matthias_freund@outlook.com>
2020-03-23 10:53:48 +0100

    - merge update
    Signed-off-by: Hego_CCTB <matthias_freund@outlook.com>

--- amalgkit/merge.py
+++ amalgkit/merge.py
@@ -2,32 +2,25 @@ import pandas
 from glob import glob
 import os
 
-
-def read_4th(fn, column, mode):
-
-    return pandas.read_csv(fn, delim_whitespace=1, usecols=[0, column], index_col=0, header=0,
-                           names=[mode, 'length_'+fn, 'eff-length_'+fn, 'est-counts_'+fn, 'tpm_'+fn])
-
-
-def exec_merge(args, mode):
-    column = None
-    if mode == 'est_counts':
-        column = 3
-    if mode == 'eff_length':
-        column = 2
-    if column is None:
-        raise NameError('mode not set correctly.')
-
-    files = glob('*.tsv')
-
-    sra_file = pandas.concat([read_4th(fn, column, mode) for fn in files], axis=1)
-    sra_file = sra_file.rename(columns={col: col.split('_')[1] for col in sra_file.columns})
-    sra_file.to_csv('./merged_file/'+mode+'_'+args.out_name+'.tsv', sep='\t')
-
-
 def merge_main(args):
+    quant_dir = os.path.join(args.work_dir, 'quant')
+    merge_dir = os.path.join(args.work_dir, 'merge')
+    if not os.path.exists(merge_dir):
+        os.makedirs(os.path.join(merge_dir))
+    sra_ids = os.listdir(quant_dir)
 
-    if not os.path.exists(os.path.join(args.work_dir+'merged_file')):
-        os.makedirs(os.path.join(args.work_dir+'merged_file'))
-    exec_merge(args, "est_counts")
-    exec_merge(args, "eff_length")
+    quant_out_paths = list()
+    for sra_id in sra_ids:
+        quant_out_path = os.path.join(quant_dir, sra_id, sra_id+'_abundance.tsv')
+        if os.path.exists(quant_out_path):
+            quant_out_paths.append(quant_out_path)
+    print('{:,} quant outfiles were detected.'.format(len(quant_out_paths)))
+    for col in ['eff_length','est_counts','tpm']:
+        out = pandas.read_csv(quant_out_paths[0], header=0, sep='\t').loc[:,['target_id',]]
+        values = [ pandas.read_csv(p, header=0, sep='\t').loc[:,[col,]] for p in quant_out_paths ]
+        for sra_id,value in zip(sra_ids,values):
+            value.columns = [sra_id,]
+        out = pandas.concat([out,]+values, axis=1, ignore_index=False, sort=False)
+        outfile = os.path.join(merge_dir, col+'.tsv')
+        print('Writing output file:', outfile)
+        out.to_csv(outfile, sep='\t', index=False)
