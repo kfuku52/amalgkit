ca6b5d4e36c2dd0a5a480e08fb126105efc16ef1
Hego_CCTB <matthias_freund@outlook.com>
2020-11-24 03:34:25 +0100

    --added Memory allocation to batch_curate.sh
    Signed-off-by: Hego_CCTB <matthias_freund@outlook.com>

--- amalgkit/batch_curate.sh
+++ amalgkit/batch_curate.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 #SBATCH --ntasks-per-node=2
 #SBATCH --time=1:00:00
+#SBATCH --mem=5GB
 
 # Run the analysis
-#wait 1
-
 Rscript ${SCRIPTPATH} ${QUANT} ${META} ${WORK} ${LEN} ${DIST} '0' ${CUT} ${INTER} ${TISSUES}
 
