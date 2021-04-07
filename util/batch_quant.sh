8671b1f926af9042873d959b4f6410a5fb09c8dc
Hego_CCTB <matthias_freund@outlook.com>
2021-04-07 16:19:30 +0200

    -- added batch support for quant
    Signed-off-by: Hego_CCTB <matthias_freund@outlook.com>

--- util/batch_quant.sh
+++ util/batch_quant.sh
@@ -1,15 +1,11 @@
 #!/bin/sh
-#$ -S /bin/bash
-#$ -cwd
-##$ -l month
-##$ -l d_rt=1400:00:00
-##$ -l s_rt=1400:00:00
-#$ -l s_vmem=4G
-#$ -l mem_req=4G
-#$ -pe def_slot 4
-#$ -t 1-2695
-
-#1-2695
+#SBATCH --job-name=amalgkit_quant   # Job name
+#SBATCH --mail-type=ALL         # Mail events (NONE, BEGIN, END, FAIL, ALL)
+#SBATCH --nodes=1                   # Use one node
+#SBATCH --ntasks=1                  # Run a single task
+#SBATCH --mem-per-cpu=1gb           # Memory per processor
+#SBATCH --time=2:00:00:00             # Time limit days:hrs:min:sec
+#SBATCH --output=array_%A-%a.out    # Standard output and error log
 
 source /home/kfuku/.bashrc
 ulimit -s unlimited
@@ -20,16 +16,13 @@ echo `date`: start
 sra_date=2018_5_1
 
 PATH=/lustre1/home/kfuku/klab/build/:${PATH}
-wd="/lustre1/home/kfuku/my_project/convergence_duplication/20180207_kallisto"
-dir_out="/lustre1/home/kfuku/my_db/Ensembl/release-91/kallisto"
-dir_sra="/lustre1/home/kfuku/ncbi/public/sra"
-dir_cdna_mask="/lustre1/home/kfuku/my_db/Ensembl/release-91/cdna.mask"
-dir_mask_fasta='/lustre1/home/kfuku/my_db/Ensembl/release-91/mask/fasta'
-sra_table="/lustre1/home/kfuku/my_db/Ensembl/release-91/sra/${sra_date}/sra_table_qualified_${sra_date}.tsv"
-key_ascp="/lustre1/home/kfuku/.aspera/connect/etc/asperaweb_id_dsa.openssh"
-
+wd=$1
+sra_table=$2
+dir_out=$3
+index_dir=$4
 overwrite=0 # 0 = False, 1 = True
-row=$[$SGE_TASK_ID+1]
+row=$SLURM_ARRAY_TASK_ID+1
+
 sci_name=`cat ${sra_table} | awk -F "\t" "NR==${row}" | awk -F "\t" '{print $1}'` ; echo sci_name: ${sci_name}
 sci_name_ub=`echo ${sci_name} | sed -e "s/[[:space:]]/_/"` ; echo sci_name_ub: ${sci_name_ub}
 index_file=`ls ${dir_cdna_mask}/${sci_name_ub}*.fasta.index` ; echo index_file: ${index_file}
@@ -38,12 +31,6 @@ sra_exp=`cat ${sra_table} | awk -F "\t" "NR==${row}" | awk -F "\t" '{print $16}'
 sra_run=`cat ${sra_table} | awk -F "\t" "NR==${row}" | awk -F "\t" '{print $17}'` ; echo sra_run: ${sra_run}
 lib_layout=`cat ${sra_table} | awk -F "\t" "NR==${row}" | awk -F "\t" '{print $27}'` ; echo lib_layout: ${lib_layout}
 nominal_length=`cat ${sra_table} | awk -F "\t" "NR==${row}" | awk -F "\t" '{print $35}' | sed -e "s/\..*//"` ; echo nominal_length: ${nominal_length}
-if [ -z "${nominal_length}" ] || [ ${nominal_length} -lt 200 ]; then
-	nominal_length=200
-fi
-echo adjusted nominal_length: ${nominal_length}
-nominal_sd=$[${nominal_length}/10]; echo nominal_sd: ${nominal_sd}
-
 # set directory
 if [ ! -e ${dir_out}/${sci_name_ub} ]; then
 	mkdir ${dir_out}/${sci_name_ub}
@@ -51,15 +38,8 @@ fi
 if [ ! -e ${dir_out}/${sci_name_ub}/${sra_run} ]; then
 	mkdir ${dir_out}/${sci_name_ub}/${sra_run}
 fi
-cd ${dir_out}/${sci_name_ub}/${sra_run}
-echo "working in: `pwd`"
-echo "`date`: start kallisto quantification"
-	if [ ${fastq_file_n} -eq 1 ]; then
-		kallisto_infiles_fastp="--single --fragment-length=${nominal_length} --sd=${nominal_sd} ${sra_run}.fastp.fastq"
-	elif [ ${fastq_file_n} -gt 1 ]; then
-		kallisto_infiles_fastp="${sra_run}_1.fastp.fastq ${sra_run}_2.fastp.fastq"
-	fi
- amalgkit quant -
+
+ amalgkit quant --ref $wd"/"$sci_name"/"$sci_name".idx" --work_dir $wd --id $sra_run
 	#kallisto quant \
 	#--index=${index_file} \
 	#--output-dir=. \
@@ -72,9 +52,9 @@ echo "`date`: start kallisto quantification"
 
 #	echo "`date`: start deleting all fastq files"
 #	rm ${sra_run}*.fastq
-else
-	echo "Output files exist already. Exiting."
-fi
+#else
+#	echo "Output files exist already. Exiting."
+#fi
 
 echo `date`: end
 
@@ -82,47 +62,47 @@ echo `date`: end
 
 
 
-: <<'#_______________CO_______________'
+#: <<'#_______________CO_______________'
 
 # single end example /usr/local/ftp/public/ddbj_database/dra/fastq/SRA142/SRA142856/SRX477531
 # paired end example /usr/local/ftp/public/ddbj_database/dra/fastq/SRA059/SRA059960/SRX196362
 
 # debug
-grep -nw ./* -e "ERROR" | sed -e "s/:.*//" | uniq
-grep -nw ./* -e "integer expression expected" | sed -e "s/:.*//" | uniq
-grep -nw ./* -e "Warning:" | sed -e "s/:.*//" | uniq
-grep -nw ./* -e "not found while parsing within network system module" | sed -e "s/:.*//" | uniq
-grep -nw ./* -e "timeout exhausted while reading file within network system module" | sed -e "s/:.*//" | uniq
-grep -nw ./* -e "path not found while resolving tree within virtual file system module" | sed -e "s/:.*//" | uniq
-grep -nw ./* -e "item not found while constructing within virtual database module" | sed -e "s/:.*//" | uniq
+#grep -nw ./* -e "ERROR" | sed -e "s/:.*//" | uniq
+#grep -nw ./* -e "integer expression expected" | sed -e "s/:.*//" | uniq
+#grep -nw ./* -e "Warning:" | sed -e "s/:.*//" | uniq
+#grep -nw ./* -e "not found while parsing within network system module" | sed -e "s/:.*//" | uniq
+#grep -nw ./* -e "timeout exhausted while reading file within network system module" | sed -e "s/:.*//" | uniq
+#grep -nw ./* -e "path not found while resolving tree within virtual file system module" | sed -e "s/:.*//" | uniq
+#grep -nw ./* -e "item not found while constructing within virtual database module" | sed -e "s/:.*//" | uniq
 
-	echo "`date`: start fastq subsampling"
-	for fastq in ${fastq_files[*]}
-	do
-		fatt_out="${fastq}_fatt_count_before_trim.tsv"
-		fatt count ${fastq} > ${fatt_out}
-		num_reads=`cat ${fatt_out} | awk -F "\t" "NR==2" | awk -F "\t" '{print $2}'`
-		avg_read_len=`cat ${fatt_out} | awk -F "\t" "NR==2" | awk -F "\t" '{print $4}'`
-		min_read_len=`cat ${fatt_out} | awk -F "\t" "NR==2" | awk -F "\t" '{print $5}'`
-		max_read_len=`cat ${fatt_out} | awk -F "\t" "NR==2" | awk -F "\t" '{print $6}'`
-		if [ ${max_read_len} -ne ${min_read_len} ]; then
-			echo "Warning: maximum read length was different from minimum read length"
-		fi
-		if [ ${num_reads} -gt ${subsample_size} ]; then
-			num_row=$[${subsample_size}*4]
-			head -${num_row} ${fastq} > ${fastq}.subsample_tmp
-			rm ${fastq}
-			mv ${fastq}.subsample_tmp ${fastq}
-		fi
-		if [ ${max_read_len} -gt ${trim_length} ]; then
-			num_trim=$[${max_read_len}-${trim_length}]
-			/lustre1/home/kfuku/bin/seqtk trimfq -e ${num_trim} ${fastq} > ${fastq}.trim_tmp
-			rm ${fastq}
-			mv ${fastq}.trim_tmp ${fastq}
-		fi
-		fatt count ${fastq} > ${fastq}_fatt_count_after_trim.tsv
-		/lustre1/home/kfuku/FastQC/fastqc --nogroup ${fastq}
-	done
+#	echo "`date`: start fastq subsampling"
+#	for fastq in ${fastq_files[*]}
+#	do
+#		fatt_out="${fastq}_fatt_count_before_trim.tsv"
+#		fatt count ${fastq} > ${fatt_out}
+#		num_reads=`cat ${fatt_out} | awk -F "\t" "NR==2" | awk -F "\t" '{print $2}'`
+#		avg_read_len=`cat ${fatt_out} | awk -F "\t" "NR==2" | awk -F "\t" '{print $4}'`
+#		min_read_len=`cat ${fatt_out} | awk -F "\t" "NR==2" | awk -F "\t" '{print $5}'`
+#		max_read_len=`cat ${fatt_out} | awk -F "\t" "NR==2" | awk -F "\t" '{print $6}'`
+#		if [ ${max_read_len} -ne ${min_read_len} ]; then
+#			echo "Warning: maximum read length was different from minimum read length"
+#		fi
+#		if [ ${num_reads} -gt ${subsample_size} ]; then
+#			num_row=$[${subsample_size}*4]
+#			head -${num_row} ${fastq} > ${fastq}.subsample_tmp
+#			rm ${fastq}
+#			mv ${fastq}.subsample_tmp ${fastq}
+#		fi
+#		if [ ${max_read_len} -gt ${trim_length} ]; then
+#			num_trim=$[${max_read_len}-${trim_length}]
+#			/lustre1/home/kfuku/bin/seqtk trimfq -e ${num_trim} ${fastq} > ${fastq}.trim_tmp
+#			rm ${fastq}
+#			mv ${fastq}.trim_tmp ${fastq}
+#		fi
+#		fatt count ${fastq} > ${fastq}_fatt_count_after_trim.tsv
+#		/lustre1/home/kfuku/FastQC/fastqc --nogroup ${fastq}
+#	done
 
 
 #_______________CO_______________
