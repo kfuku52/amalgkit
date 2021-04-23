118041894f445bfdac8dd6dc093aa6e01c4663af
Hego_CCTB <matthias_freund@outlook.com>
2021-04-23 13:13:38 +0200

    --added more stats to getfastq metadata output
    Signed-off-by: Hego_CCTB <matthias_freund@outlook.com>

--- amalgkit/getfastq.py
+++ amalgkit/getfastq.py
@@ -507,6 +507,9 @@ def dump_read_stats(args, metadata, seq_summary, output_dir, sra_id):
         metadata.df.loc[metadata.df['run'] == sra_id, 'num_read_fastp_input'] = seq_summary['bp_fastp_in'].sum()
         metadata.df.loc[metadata.df['run'] == sra_id, 'num_read_fastp'] = seq_summary['bp_fastp_out'].sum()
     metadata.df.loc[metadata.df['run'] == sra_id, 'num_read_unfiltered'] = metadata.df.loc[metadata.df['run'] == sra_id, 'total_spots']
+    metadata.df.loc[metadata.df['run'] == sra_id, 'spot_length'] = seq_summary['spot_length'].loc[sra_id]
+    metadata.df.loc[metadata.df['run'] == sra_id, 'total_spot'] = seq_summary['total_spot'].loc[sra_id]
+
     metadata.df.to_csv(os.path.join(output_dir, 'metadata_' + sra_id + '.tsv'), sep='\t', index=False)
 
 
