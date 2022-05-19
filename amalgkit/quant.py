cfaa222053616ed77ae3e66833861a626842ce53
Hego_CCTB <matthias_freund@outlook.com>
2022-05-19 11:36:49 +0200

    - quant now skips SRAID, if it can't find the qetfastq output, instead of terminating
    Signed-off-by: Hego_CCTB <matthias_freund@outlook.com>

--- amalgkit/quant.py
+++ amalgkit/quant.py
@@ -1,16 +1,13 @@
-import numpy
-
-import glob
 import subprocess
-import sys
 from amalgkit.util import *
 
+
 def check_quant_output(sra_id, output_dir, args):
-    out_path = os.path.join(output_dir, sra_id+'_abundance.tsv')
+    out_path = os.path.join(output_dir, sra_id + '_abundance.tsv')
     is_output_present = os.path.exists(out_path)
     if is_output_present:
         print('Output file detected: {}'.format(out_path))
-        if (args.redo=='yes'):
+        if args.redo == 'yes':
             print('Continued. The output will be overwritten. Set "--redo no" to exit.')
             return None
         else:
@@ -20,16 +17,18 @@ def check_quant_output(sra_id, output_dir, args):
         print('Output file was not detected: {}'.format(out_path))
         return None
 
+
 def call_kallisto(args, in_files, metadata, sra_stat, output_dir, index):
     sra_id = sra_stat['sra_id']
     lib_layout = sra_stat['layout']
+    kallisto_cmd = ''
     if lib_layout == 'single':
         print("single end reads detected. Proceeding in single mode")
-        if len(in_files)!=1:
+        if len(in_files) != 1:
             txt = "Library layout: {} and expected 1 input file. " \
                   "Received {} input file[s]. Please check your inputs and metadata."
             raise ValueError(txt.format(lib_layout, len(in_files)))
-        nominal_length = metadata.df.loc[:,'nominal_length'].values[0]
+        nominal_length = metadata.df.loc[:, 'nominal_length'].values[0]
         if nominal_length:
             print('Nominal length in metadata is unusually small ({}). Setting it to 200.'.format(nominal_length))
             if nominal_length < 200 or numpy.isnan(nominal_length):
@@ -41,15 +40,15 @@ def call_kallisto(args, in_files, metadata, sra_stat, output_dir, index):
         fragment_sd = nominal_length / 10
         print("Fragment length standard deviation set to: {}".format(fragment_sd))
         kallisto_cmd = ["kallisto", "quant", "--threads", str(args.threads), "--index", index, "-o", output_dir,
-                       "--single", "-l", str(nominal_length), "-s", str(fragment_sd), in_files[0]]
+                        "--single", "-l", str(nominal_length), "-s", str(fragment_sd), in_files[0]]
     elif lib_layout == 'paired':
-        if len(in_files)!=2:
+        if len(in_files) != 2:
             txt = "Library layout: {} and expected 2 input files. " \
                   "Received {} input file[s]. Please check your inputs and metadata."
             raise ValueError(txt.format(lib_layout, len(in_files)))
         print("paired end reads detected. Running in paired read mode.")
         kallisto_cmd = ["kallisto", "quant", "--threads", str(args.threads), "-i", index, "-o",
-                       output_dir, in_files[0], in_files[1]]
+                        output_dir, in_files[0], in_files[1]]
 
     print('Command: {}'.format(' '.join(kallisto_cmd)))
     kallisto_out = subprocess.run(kallisto_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -57,7 +56,7 @@ def call_kallisto(args, in_files, metadata, sra_stat, output_dir, index):
     print(kallisto_out.stdout.decode('utf8'))
     print('kallisto stderr:')
     print(kallisto_out.stderr.decode('utf8'))
-    if (kallisto_out.returncode != 0):
+    if kallisto_out.returncode != 0:
         sys.stderr.write("kallisto did not finish safely.\n")
         if 'zero reads pseudoaligned' in kallisto_out.stderr.decode('utf8'):
             sys.stderr.write('No reads are mapped to the reference. This sample will be removed by `amalgkit curate`.')
@@ -72,20 +71,23 @@ def call_kallisto(args, in_files, metadata, sra_stat, output_dir, index):
 
     return kallisto_out
 
+
 def check_kallisto_dependency():
     try:
         subprocess.run(['kallisto', '-h'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    except FileNotFoundError as e:
+    except FileNotFoundError:
         raise Exception("kallisto is not installed.")
 
+
 def check_layout_mismatch(sra_stat, output_dir):
-    if (sra_stat['layout'] == 'paired'):
-        fastq_files = glob.glob(os.path.join(output_dir, sra_stat['sra_id']+'*.fastq*'))
-        if (len(fastq_files)==1):
+    if sra_stat['layout'] == 'paired':
+        fastq_files = glob.glob(os.path.join(output_dir, sra_stat['sra_id'] + '*.fastq*'))
+        if len(fastq_files) == 1:
             sys.stderr.write('Single-end fastq was detected even though layout = {}\n'.format(sra_stat['layout']))
             sys.stderr.write('This sample will be treated as single-end sequencing.\n')
             sra_stat['layout'] = 'single'
     return sra_stat
+
 
 def run_quant(args, metadata, sra_id, index):
     # make results directory, if not already there
@@ -98,7 +100,12 @@ def run_quant(args, metadata, sra_id, index):
     output_dir_getfastq = os.path.join(args.out_dir, 'getfastq', sra_id)
     sra_stat = get_sra_stat(sra_id, metadata, num_bp_per_sra=None)
     sra_stat = check_layout_mismatch(sra_stat, output_dir_getfastq)
-    ext = get_newest_intermediate_file_extension(sra_stat, work_dir=output_dir_getfastq)
+    try:
+        ext = get_newest_intermediate_file_extension(sra_stat, work_dir=output_dir_getfastq)
+    except FileNotFoundError:
+        print('could not find directory:', output_dir_getfastq)
+        print('skipping')
+        return
     in_files = glob.glob(os.path.join(args.out_dir, 'getfastq', sra_id, sra_id + "*" + ext))
 
     # start quantification process.
@@ -108,13 +115,14 @@ def run_quant(args, metadata, sra_id, index):
 
     call_kallisto(args, in_files, metadata, sra_stat, output_dir, index)
 
-    if (args.clean_fastq=='yes')&(os.path.exists(os.path.join(output_dir, sra_id + "_abundance.tsv"))):
+    if (args.clean_fastq == 'yes') & (os.path.exists(os.path.join(output_dir, sra_id + "_abundance.tsv"))):
         for in_file in in_files:
             print('Output file detected. Safely removing fastq:', in_file)
             os.remove(in_file)
-            placeholder = open(in_file+'.safely_removed', "w")
+            placeholder = open(in_file + '.safely_removed', "w")
             placeholder.write("This fastq file was safely removed after `amalgkit quant`.")
             placeholder.close()
+
 
 def get_index(args, sci_name):
     if args.index_dir is not None:
@@ -134,9 +142,10 @@ def get_index(args, sci_name):
     print("Index file found: {}".format(index))
     return index
 
+
 def quant_main(args):
     check_kallisto_dependency()
-    metadata = load_metadata(args) # loads single-row metadata according to --batch
+    metadata = load_metadata(args)  # loads single-row metadata according to --batch
     if args.id is None:
         for i in metadata.df.index:
             print('')
