a42d5e612415b42b3be4c969cddc0e379fb7fcd3
Hego_CCTB <matthias_freund@outlook.com>
2023-01-16 15:19:26 +0100

    - changed --config_dir to --config_name to distinguish usage
    Signed-off-by: Hego_CCTB <matthias_freund@outlook.com>

--- amalgkit/config.py
+++ amalgkit/config.py
@@ -4,41 +4,41 @@ from glob import glob
 
 
 def check_directory(args):
-    if args.config_dir == 'inferred':
-        config_dir = args.config
+    if args.config_name == 'inferred':
+        config_name = args.config
     else:
-        config_dir = args.config_dir
+        config_name = args.config_name
     work_dir = os.path.join(args.out_dir, 'config')
-    path_config_dir = os.path.join(work_dir, config_dir)
+    path_config_name = os.path.join(work_dir, config_name)
 
     if not os.path.exists(os.path.join(work_dir)):
         os.makedirs(os.path.join(work_dir))
 
     print('Checking config directory ...')
-    if os.path.exists(path_config_dir):
-        print(path_config_dir, ' already exists.')
+    if os.path.exists(path_config_name):
+        print(path_config_name, ' already exists.')
         if args.overwrite:
-            print('--overwrite is set! Any config files in ', path_config_dir, ' will be overwritten.')
+            print('--overwrite is set! Any config files in ', path_config_name, ' will be overwritten.')
             print('Continuing.')
         else:
             print('Exiting.')
             sys.exit()
     else:
-        os.makedirs(path_config_dir)
+        os.makedirs(path_config_name)
 
     return
 
 
 def check_files(args, config_file_list, ext='.config'):
     work_dir = os.path.join(args.out_dir, 'config')
-    print('Checking if all config files are present in ', os.path.join(work_dir, args.config_dir), '...')
+    print('Checking if all config files are present in ', os.path.join(work_dir, args.config_name), '...')
     missing_files = []
     for config_file in config_file_list:
-        path_config_file = os.path.join(work_dir, args.config_dir, config_file + ext)
+        path_config_file = os.path.join(work_dir, args.config_name, config_file + ext)
         if os.path.exists(path_config_file):
             print('found ', config_file + ext, '...')
         else:
-            print('could not find ', config_file + ext, 'in ', os.path.join(work_dir, args.config_dir))
+            print('could not find ', config_file + ext, 'in ', os.path.join(work_dir, args.config_name))
             print('make sure amalgkit config ran correctly')
             missing_files.append(config_file + ext)
     if len(missing_files) > 0:
@@ -52,10 +52,10 @@ def check_files(args, config_file_list, ext='.config'):
 
 def create_config_from_package(args):
 
-    if args.config_dir == 'inferred':
+    if args.config_name == 'inferred':
         config_source = args.config
     else:
-        config_source = args.config_dir
+        config_source = args.config_name
 
     work_dir = os.path.join(args.out_dir, 'config')
     path_config = os.path.join(work_dir, config_source)
@@ -66,12 +66,12 @@ def create_config_from_package(args):
         # Try backported to PY<37 `importlib_resources`.
         import importlib_resources as ir
 
-    config_base = 'amalgkit.config_dir' + '.' + args.config
+    config_base = 'amalgkit.config_name' + '.' + args.config
     config_files = ir.files(config_base).rglob('*.config')
 
     for config_file in config_files:
         try:
-            # extracts the contents of a single file from the config_dir within the amalgkit package
+            # extracts the contents of a single file from the config_name within the amalgkit package
             file_content = ir.files(config_base).joinpath(os.path.basename(config_file)).read_bytes()
         except:
             continue
