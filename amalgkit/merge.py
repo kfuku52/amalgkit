44991794c5355daa5f9dcc4571bba849d568a200
Kenji Fukushima <kfuku52@gmail.com>
2023-11-20 22:06:22 +0100

    Supported merge plots

--- amalgkit/merge.py
+++ amalgkit/merge.py
@@ -5,30 +5,30 @@ import warnings
 from amalgkit.util import *
 
 def merge_main(args):
-    quant_dir = os.path.join(args.out_dir, 'quant')
-    merge_dir = os.path.join(args.out_dir, 'merge')
+    quant_dir = os.path.realpath(os.path.join(args.out_dir, 'quant'))
+    merge_dir = os.path.realpath(os.path.join(args.out_dir, 'merge'))
     if not os.path.exists(merge_dir):
         os.makedirs(os.path.join(merge_dir))
     metadata = load_metadata(args)
     spp = metadata.df.loc[:,'scientific_name'].dropna().unique()
-
     for sp in spp:
         print('processing: {}'.format(sp), flush=True)
         sp_filled = sp.replace(' ', '_')
         merge_species_dir = os.path.join(os.path.join(merge_dir, sp_filled))
-        if not os.path.exists(merge_species_dir):
-            os.makedirs(merge_species_dir)
         is_sp = (metadata.df.loc[:,'scientific_name']==sp)
         sra_ids = metadata.df.loc[is_sp,'run'].values
-        is_sampled = metadata.df.loc[:,'is_sampled']=='yes'
+        is_sampled = (metadata.df.loc[:,'exclusion']=='no')
         sampled_sra_ids = metadata.df.loc[is_sampled,'run'].values
         if len(sra_ids)==0:
-            warnings.warn('No SRA Run ID found: {}'.format(sp))
+            warnings.warn('No SRA Run ID found. Skipping: {}'.format(sp))
+            continue
         quant_out_paths = list()
         detected_sra_ids = list()
         for sra_id in sra_ids:
             quant_out_path = os.path.join(quant_dir, sra_id, sra_id+'_abundance.tsv')
             if os.path.exists(quant_out_path):
+                if not os.path.exists(merge_species_dir):
+                    os.makedirs(merge_species_dir)
                 quant_out_paths.append(quant_out_path)
                 detected_sra_ids.append(sra_id)
             else:
@@ -48,4 +48,10 @@ def merge_main(args):
             print('Writing output file:', outfile)
             out.to_csv(outfile, sep='\t', index=False)
     print('Getting mapping rate from quant output and write new metadata file into merge directory.', flush=True)
-    write_updated_metadata(metadata, os.path.join(args.out_dir, 'merge', 'metadata.tsv'), args)
+    path_metadata_merge = os.path.realpath(os.path.join(args.out_dir, 'merge', 'metadata.tsv'))
+    write_updated_metadata(metadata, path_metadata_merge, args)
+    r_merge_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'merge.r')
+    r_util_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'util.r')
+    r_command = ['Rscript', r_merge_path, merge_dir, path_metadata_merge, r_util_path]
+    print('Starting R script for plot generation: {}'.format(' '.join(r_command)), flush=True)
+    subprocess.check_call(r_command)
