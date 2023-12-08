b3a519beb24de53dc4e9e2d5c14f4f6abb3de642
Kenji Fukushima <kfuku52@gmail.com>
2023-12-08 10:00:02 +0100

    removed unnecessary file

--- amalgkit/batch_curate.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#SBATCH --ntasks-per-node=2
-#SBATCH --time=1:00:00
-#SBATCH --mem=5GB
-
-# Run the analysis
-Rscript ${SCRIPTPATH} ${QUANT} ${META} ${WORK} ${LEN} ${DIST} '0' ${CUT} ${INTER} ${TISSUES}
-
